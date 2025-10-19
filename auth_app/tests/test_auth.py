f3a48d202178e7eadd46797ff611ebf54a95e26b
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-20 00:19:33 +0200

    Test: RegisertTests class refactored.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -8,10 +8,10 @@ class RegisterTests(APITestCase):
 
     def get_request_data(self, **overrides):
         base = {
-            "username": "test_username",
-            "password": "test_password",
-            "confirmed_password": "test_password",
-            "email": "test_email@example.com"
+            'username': "test_username",
+            'password': "test_password",
+            'confirmed_password': "test_password",
+            'email': "test_email@example.com"
         }
         base.update(overrides)
         return base
@@ -21,30 +21,29 @@ class RegisterTests(APITestCase):
         self.username = 'TEST_USER'
         self.email = 'TEST@USER.de'
         self.User = get_user_model()
-        self.request_data = self.get_request_data()
-        self.request_data_wrong_password = self.get_request_data(confirmed_password='wrong')
-        self.request_data_wrong_email = self.get_request_data(email=self.email)
-        self.request_data_wrong_username = self.get_request_data(username=self.username)
         self.url = reverse('register')
         self.user = self.User.objects.create_user(username=self.username, email=self.email, password='TEST1234')
 
 
     def test_post_success(self):
         expected_fields = {'detail'}
-        response = self.client.post(self.url, self.request_data, format='json')
+        response = self.client.post(self.url, self.get_request_data(), format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(set(response.data.keys()), expected_fields)
+        self.assertTrue(self.User.objects.filter(username=self.get_request_data()['username']).exists())
+        self.assertEqual(self.User.objects.filter(username=self.get_request_data()['username']).count(), 1)
 
 
     def test_post_fails(self):
         cases = [
-            self.request_data_wrong_username,
-            self.request_data_wrong_email,
-            self.request_data_wrong_password,
+            ("duplicate_username", self.get_request_data(username=self.username)),
+            ("duplicate_email", self.get_request_data(email=self.email)),
+            ("password_mismatch", self.get_request_data(confirmed_password="wrong"))
         ]
 
-        for data in cases:
-            response = self.client.post(self.url, data, format='json')
+        for message, data in cases:
+            with self.subTest(test_case=message):
+                response = self.client.post(self.url, data, format='json')
 
-            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+                self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)

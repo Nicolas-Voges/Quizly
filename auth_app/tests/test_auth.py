c9b4e29c42cb50466e1bfb8db2d468213b2f714d
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-20 01:36:35 +0200

    Test: LoginTests class added.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -47,3 +47,39 @@ class RegisterTests(APITestCase):
                 response = self.client.post(self.url, data, format='json')
 
                 self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class LoginTests(APITestCase):
+
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'test1234'
+        self.User = get_user_model()
+        self.user = self.User.objects.create_user(username=self.username, password=self.password)
+        self.url = reverse('token_obtain_pair')
+
+    def test_post_success(self):
+        data = {
+            'username': self.username,
+            'password': self.password
+        }
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.cookies)
+        self.assertIn('refresh', response.cookies)
+
+
+    def test_post_fails(self):
+        cases = [
+            ("wrong_username", {'username': self.username, 'password': 'wrong_password'}),
+            ("wrong_password", {'username': 'wrong', 'password': self.password})
+        ]
+
+        for message, data in cases:
+            with self.subTest(test_case=message):
+                response = self.client.post(self.url, data, format='json')
+
+                self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+                self.assertNotIn('access', response.cookies)
+                self.assertNotIn('refresh', response.cookies)

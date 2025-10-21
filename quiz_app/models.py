e5a6cdf26e0e34aa0c6df2ad4610dbfbed9cdcc6
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-21 16:00:05 +0200

    Feat: Quiz and Question models added.

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=63)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    video_url = models.URLField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Quiz {self.id} {self.title}  by {self.creator.username}"
+    
+
+class Question(models.Model):
+    question_title = models.CharField(max_length=255)
+    question_options = models.JSONField()
+    answer = models.CharField(max_length=255)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+    
+    def __str__(self):
+        return f"Question {self.id} for Quiz {self.quiz.id}"

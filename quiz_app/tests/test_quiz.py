221ffaa222f3d4d3e3d41509b563dbd4aaa0ad07
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-21 15:53:53 +0200

    Test: Refactor tests in QuizTests class.

--- quiz_app/tests/test_quiz.py
+++ quiz_app/tests/test_quiz.py
@@ -14,12 +14,15 @@ class QuizTests(APITestCase):
 
     def setUp(self):
         self.video_url = "https://www.youtube.com/watch?v=P8zzrqLEvoI"
+        self.post_data = {'video_url': self.video_url}
+        self.user = User.objects.create_user(username="username", email="te@st.mail", password='TEST1234')
         self.quiz = Quiz.objects.create(
             title="Sample Quiz",
             description="A simple quiz for testing.",
             created_at=timezone.now(),
             updated_at=timezone.now(),
-            video_url=self.video_url
+            video_url=self.video_url,
+            creator=self.user.pk
         )
         self.questions = [
             Question.objects.create(
@@ -34,20 +37,15 @@ class QuizTests(APITestCase):
         self.url_list = reverse('quizzes-list')
         self.url_create = reverse('create-quiz')
         self.url_detail = reverse('quizzes-detail', kwargs={'pk': self.quiz.pk})
-        self.user = User.objects.create_user(username="username", email="te@st.mail", password='TEST1234')
-
+        self.expected_fields = {'id', 'title', 'description', 'created_at', 'updated_at', 'video_url', 'questions'}
 
     def test_post_success(self):
-        expected_fields = {'id', 'title', 'description', 'created_at', 'updated_at', 'video_url', 'questions'}
-        data = {
-            "video_url": self.video_url
-        }
         self.client.post(self.url_login, {'username': 'username', 'password': 'TEST1234'}, format='json')
-        response = self.client.post(self.url_create, data, format='json')
+        response = self.client.post(self.url_create, self.post_data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(set(response.data.keys()), expected_fields)
-        self.assertTrue(Quiz.objects.filter(title=data['title']).exists())
+        self.assertEqual(set(response.data.keys()), self.expected_fields)
+        self.assertTrue(Quiz.objects.filter(title=self.post_data['title']).exists())
 
 
     def test_post_fails(self):

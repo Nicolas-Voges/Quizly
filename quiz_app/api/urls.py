cd907ea186e39a66cef294074108ebc3503e8f51
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-23 20:16:12 +0200

    Feat: Quiz URLs added.

--- quiz_app/api/urls.py
+++ quiz_app/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework import routers
 
-from .views import CreateQuizAPIView
-
+from .views import CreateQuizAPIView, QuizListAPIView, QuizRetrieveUpdateDestroyAPIView
+router = routers.DefaultRouter()
 urlpatterns = [
-    path('createQuiz/', CreateQuizAPIView.as_view(), name='create-quiz')
+    path('createQuiz/', CreateQuizAPIView.as_view(), name='create-quiz'),
+    path('quizzes/', QuizListAPIView.as_view(), name='quizzes-list'),
+    path('quizzes/<int:pk>/', QuizRetrieveUpdateDestroyAPIView.as_view(), name='quizzes-detail')
 ]

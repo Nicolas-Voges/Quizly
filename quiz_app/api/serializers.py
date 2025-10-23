22d04198c968596666fd192d52b455f058463392
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-23 12:15:18 +0200

    Feat: QuestionSerializer added.

--- quiz_app/api/serializers.py
+++ quiz_app/api/serializers.py
@@ -2,6 +2,26 @@ from rest_framework import serializers
 
 from quiz_app.models import Quiz, Question
 
+class QuestionSerializer(serializers.ModelSerializer):
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Question
+        fields = ['id', 'question_title', 'question_options', 'answer', 'created_at', 'updated_at']
+
+
+    def format_datetime(self, dt):
+        return dt.strftime("%Y-%m-%dT%H:%M:%S.") + f"{int(dt.microsecond / 1000):03d}Z"
+
+
+    def get_created_at(self, obj):
+        return self.format_datetime(obj.created_at)
+    
+    
+    def get_updated_at(self, obj):
+        return self.format_datetime(obj.updated_at)
+
 
 class QuizPostSerializer(serializers.ModelSerializer):
     created_at = serializers.SerializerMethodField()
